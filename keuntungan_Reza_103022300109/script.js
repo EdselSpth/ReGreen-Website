f1faa5ec6574974307a311e46f4b7d79803b6a4a
RezaIndraMaulana <rezaindramaulana9@gmail.com>
2025-11-02 10:38:18 +0700

    [RZZ] - membuat data keuntungan pakai JSON
    mengubah data yang ada pada data keuntungan jadi menggunakan JSON

--- keuntungan_Reza_103022300109/script.js
+++ keuntungan_Reza_103022300109/script.js
@@ -4,3 +4,27 @@ document.getElementById("logoutBtn").addEventListener("click", function (e) {
     window.location.href = "../login_Edsel_103022300016/login.html";
   }
 });
+
+fetch("../keuntungan_Reza_103022300109/dataKeuntungan.json")
+  .then(response => response.json())
+  .then(data => {
+    const tbody = document.getElementById("tableBody");
+    tbody.innerHTML = "";
+
+    data.forEach(item => {
+      const row = document.createElement("tr");
+      row.innerHTML = `
+        <td>${item.id}</td>
+        <td>${item.nama}</td>
+        <td>${item.nominal}</td>
+        <td>${item.rekening}</td>
+        <td>${item.metode}</td>
+        <td>
+          <button class="btn btn-terima">Terima</button>
+          <button class="btn btn-tolak">Tolak</button>
+        </td>
+      `;
+      tbody.appendChild(row);
+    });
+  })
+  .catch(err => console.error("Gagal load data JSON:", err));
