82d311227040471e7a23689e45483bf62579d412
KuroAkai <derudj12345@gmail.com>
2025-11-02 16:00:44 +0700

    FIX: [DKD] navbar dan btn logout

--- bankSampah_Deru_103022300101/script.js
+++ bankSampah_Deru_103022300101/script.js
@@ -109,3 +109,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
   renderTabel();
 });
+document.getElementById("logoutBtn").addEventListener("click", function (e) {
+  e.preventDefault();
+  if (confirm("Apakah Anda yakin ingin logout?")) {
+    window.location.href = "../login_Edsel_103022300016/login.html";
+  }
+});
